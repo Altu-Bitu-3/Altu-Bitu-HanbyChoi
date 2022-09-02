5bfc022954cec116b6b44c40ac16a383aa3d77c2
Hanby Choi <hanby.choi@ewhain.net>
2022-09-03 07:28:48 +0900

    10430 나머지

---
+++ 백준 c++ 기초/10430.cpp
@@ -0,0 +1,8 @@
+# include <iostream>
+using namespace std;
+int a, b, c;
+int main(){
+    cin >> a >> b >> c;
+    cout << (a+b)%c << "\n" << ((a%c)+(b%c))%c << "\n" << (a*b)%c << "\n" << ((a%c)*(b%c))%c;
+    return 0;
+}
