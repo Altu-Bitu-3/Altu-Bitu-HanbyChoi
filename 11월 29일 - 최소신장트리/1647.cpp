6aade136006acafaa16e0d24c4983acbe6c27a8e
Hanby Choi <hanby.choi@ewhain.net>
2022-12-19 12:04:04 +0900

    [최소 신장 트리] 12월 19일 과제 수정

--- 11월 29일 - 최소신장트리/1647.cpp
+++ 11월 29일 - 최소신장트리/1647.cpp
@@ -9,7 +9,7 @@ typedef tuple<int, int, int> tp;
 vector<int> parent;
 
 int findParent(int x){
-    if (parent[x] == x){
+    if (parent[x] < 0){
         return x;
     }
     return parent[x] = findParent(parent[x]);
@@ -32,16 +32,18 @@ bool unionNodes(int x, int y){
 }
 
 int kruskal(int n, vector<tp> &vertex){
-    int sum = 0, cnt = 0, max_sum = 0;
+    int sum = 0, cnt = 0, max_cost = 0;
     for (auto [w, n1, n2]:vertex){
-        if (!unionNodes(n1, n2)){ // 부모가 다르면
+        if (!unionNodes(n1, n2)){ // 부모가 같으면
             continue;
         }
-        sum += w;
+        // 부모가 다르면
+        sum += w; // 가중치 합산
+        max_cost = max(max_cost, w);
         cnt++;
 
         if (cnt == n-1){ // 필요한 간선을 모두 찾은 경우
-            return sum; // 가중치의 합 반환
+            return (sum - max_cost); // 가중치의 합 반환
         }
     }
     return 0; // ST 못 만든 경우
